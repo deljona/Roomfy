e66fd6bfc3dafdea80cb3551550241823cefca27
Jonathan <jdrx94@outlook.com>
2023-04-01 20:54:56 +0200

    Fix Connection

--- roomr/chat/lib/services/client.dart
+++ roomr/chat/lib/services/client.dart
@@ -6,11 +6,12 @@ var logger = Logger(printer: PrettyPrinter());
 
 class Client {
   static void connectToSocket({required String ip, required String port}) {
-    var uri = "http://$ip:$port" as Uri;
-    io.Socket socket = io.io(uri);
+    var uri = "http://$ip:$port";
+    io.Socket socket =
+        io.io(uri, io.OptionBuilder().setTransports(['websocket']).build());
     socket.onConnect((_) {
       logger.i("Conectado al Socket!");
-      socket.emit('mensaje', 'Hola server!');
+      socket.emit('mensaje', 'Usuario conectado.');
     });
   }
 }
