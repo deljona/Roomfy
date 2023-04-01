373f7e2322861ec315034536df58f9378d6f7ee3
Jonathan <jdrx94@outlook.com>
2023-04-01 20:14:37 +0200

    first commit

---
+++ server/ServerIO.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from cryptography.fernet import Fernet
+import base64
+
+key = Fernet.generate_key()
+__SECRET_KEY = base64.urlsafe_b64encode(key).decode()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = __SECRET_KEY
+socketio = SocketIO(app)
+
+@socketio.on('mensaje')
+def handle_message(mensaje):
+    print('Mensaje recibido: ' + mensaje)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
