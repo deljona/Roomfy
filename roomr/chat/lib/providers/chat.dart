842dad5dfeabdb1175df8cb0205aadb71aff9ade
Jonathan <jdrx94@outlook.com>
2023-04-28 19:00:29 +0200

    Adding providers and bubble

---
+++ roomr/chat/lib/providers/chat.dart
@@ -0,0 +1,14 @@
+import 'package:chat/models/message.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class ChatProvider extends ChangeNotifier {
+  final List<Message> _messages = [];
+
+  List<Message> get messages => _messages;
+
+  addNewMessage(Message message) {
+    _messages.add(message);
+    notifyListeners();
+  }
+}
---
+++ roomr/chat/lib/providers/login.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+
+class LoginProvider extends ChangeNotifier {
+  String _errorMessage = '';
+
+  String get errorMessage => _errorMessage;
+
+  setErrorMessage(String message) {
+    _errorMessage = message;
+    notifyListeners();
+  }
+}
